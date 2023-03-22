4dda264771c5f166e6153e59fcb75d767efd9ee0
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2023-03-21 22:10:27 -0400

    Reformat api

--- api.py
+++ api.py
@@ -6,29 +6,51 @@ jsfunction = str
 token = str
 data = str
 
-def create_data(code: jsfunction, url: str, title: str, author: str) -> data:
-    return f'{{"code":"{code}","url":"{url}","title":"{title}","author":"{author}"}}'
+class Blinken:
+    @staticmethod
+    def from_components(code: jsfunction, url: str, title: str, author: str) -> data:
+        return Blinken(f'{{"code":{code},"url":"{url}","title":"{title}","author":"{author}"}}')
 
-def set(data: data) -> token:
-    return requests.post(url="https://blinken.org/api/0/publish", data=data, headers=_header).text
+    def __init__(self, data: data) -> None:
+        res = requests.post(url="https://blinken.org/api/0/publish", data=data, headers=_header)
+        if 200 <= res.status_code < 400:
+            self.token = res.text
+        else:
+            raise ValueError("Unexpected status code", res)
 
-def get(token: token) -> requests.Response:
-    return requests.get(f'https://blinken.org/api/0/status/{token}', headers=_header)
+    def get_status(self) -> requests.Response:
+        return requests.get(f'https://blinken.org/api/0/status/{self.token}', headers=_header)
 
-def cancel(token: token) -> requests.Response:
-    return requests.post(f'https://blinken.org/api/0/cancel/{token}', data='{}', headers=_header)
+    def cancel(self) -> requests.Response:
+        return requests.post(f'https://blinken.org/api/0/cancel/{self.token}', data='{}', headers=_header)
 
 if __name__ == '__main__':
-    default = create_data(
-        r'function () {\n    // My self-contained program goes here.    \n    var x = 0;\n'
-        r'    var d = 1;\n    var n = 10;\n\n    // Update lights one frame\n    return function'
-        r' (lights) {\n        for (i = 0; i < lights.length; i++) {\n            '
-        r'if (i > x - n && i < x + n) {\n                lights[i].rgb(1, 0, 0);\n            } '
-        r'else {\n                lights[i].rgb(0, 0, 0);\n            }\n        }\n        x += d;'
-        r'\n        if (x == lights.length+n || x == -n) {\n            d *= -1;\n        }\n        '
-        r'return 50; // ms until called again\n    };\n}',
+    import json
+
+    default = Blinken.from_components(
+        # javascript
+        json.dumps('''
+        function () {
+            // My self-contained program goes here.
+            var x = 0;
+            var d = 1;
+            var n = 10;
+            // Update lights one frame
+            return function (lights) {
+                for (i = 0; i < lights.length; i++) {
+                    if (i > x - n && i < x + n) {
+                        lights[i].rgb(1, 0, 0);
+                    } else {
+                        lights[i].rgb(0, 0, 0);
+                    }
+                }
+                x += d;
+                if (x == lights.length+n || x == -n) {
+                    d *= -1;
+                }
+                return 50; // ms until called again
+            };
+        }'''),
         'https://fiddle.jshell.net/aLgm721f/show/light/',
         'Red Snake',
         'Anonymous')
-
-    token = set(default)
